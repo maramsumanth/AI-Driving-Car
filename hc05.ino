dd25a87a28362595d2e035a74c59dbf43a70465a
Maram Sumanth <32808381+maramsumanth@users.noreply.github.com>
2019-01-20 22:57:28 +0530

    Add files via upload

---
+++ hc05.ino
@@ -0,0 +1,92 @@
+int motorLpin1=2;
+int motorLpin2=3;
+int motorRpin1=4;
+int motorRpin2=5;
+int motorLpwm=10;
+int motorRpwm=11;
+
+int motorSpeed=125;
+int turn=50;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.flush();
+  pinMode(motorLpin1,OUTPUT);
+  pinMode(motorLpin2,OUTPUT);
+  pinMode(motorRpin1,OUTPUT);
+  pinMode(motorRpin2,OUTPUT);
+  pinMode(motorLpwm,OUTPUT);
+  pinMode(motorRpwm,OUTPUT);
+}
+
+void loop() {
+  String input="";
+  while(Serial.available()){
+    input+=(char)Serial.read();
+    delay(5);
+  }
+  
+  if(input=="n"){
+    stp();
+  }
+  else if(input=="F"){
+    fwd();
+  }
+  else if(input=="R"){
+    rev();
+  }
+  else if(input.indexOf("TL")>-1){
+    lft();
+  }
+  else if(input.indexOf("TR")>-1){
+    rght();
+  }
+  else if(input!=""){
+    motorSpeed=input.toInt();
+  }
+}
+
+void fwd(){
+  analogWrite(motorLpwm,motorSpeed);
+  analogWrite(motorRpwm,motorSpeed);
+  digitalWrite(motorLpin1,1);
+  digitalWrite(motorLpin2,0);
+  digitalWrite(motorRpin1,1);
+  digitalWrite(motorRpin2,0);
+}
+
+void rev(){
+  analogWrite(motorLpwm,motorSpeed);
+  analogWrite(motorRpwm,motorSpeed);
+  digitalWrite(motorLpin1,0);
+  digitalWrite(motorLpin2,1);
+  digitalWrite(motorRpin1,0);
+  digitalWrite(motorRpin2,1);
+}
+
+void lft(){
+  analogWrite(motorLpwm,motorSpeed-turn);
+  analogWrite(motorRpwm,motorSpeed+turn);
+  digitalWrite(motorLpin1,0);
+  digitalWrite(motorLpin2,1);
+  digitalWrite(motorRpin1,1);
+  digitalWrite(motorRpin2,0);
+}
+
+void rght(){
+  analogWrite(motorLpwm,motorSpeed+turn);
+  analogWrite(motorRpwm,motorSpeed-turn);
+  digitalWrite(motorLpin1,1);
+  digitalWrite(motorLpin2,0);
+  digitalWrite(motorRpin1,0);
+  digitalWrite(motorRpin2,1);
+}
+
+void stp(){
+  analogWrite(motorLpwm,0);
+  analogWrite(motorRpwm,0);
+  digitalWrite(motorLpin1,1);
+  digitalWrite(motorLpin2,1);
+  digitalWrite(motorRpin1,1);
+  digitalWrite(motorRpin2,1);
+}
---
+++ sensor.ino
@@ -0,0 +1,24 @@
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin, INPUT); 
+Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(1000000);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(1000000);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+distance= duration*0.034/2;
+Serial.print("Distance: ");
+Serial.println(distance);
+}
