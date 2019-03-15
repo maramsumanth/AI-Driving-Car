686d5c02ff3d6e7cdb2932eb2772fe140a161e1a
Maram Sumanth <maram.sumanth@gmail.com>
2019-03-15 18:30:06 +0530

    Update and rename sensor.ino to testsensors.ino

--- testsensors.ino
+++ testsensors.ino
@@ -2,20 +2,21 @@ const int trigPin = 9;
 const int echoPin = 10;
 long duration;
 int distance;
+
 void setup() {
-pinMode(trigPin, OUTPUT); 
-pinMode(echoPin, INPUT); 
-Serial.begin(9600); // Starts the serial communication
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+Serial.begin(9600);
 }
 void loop() {
 
 digitalWrite(trigPin, LOW);
 delayMicroseconds(1000000);
-// Sets the trigPin on HIGH state for 10 micro seconds
+
 digitalWrite(trigPin, HIGH);
 delayMicroseconds(1000000);
 digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
+
 duration = pulseIn(echoPin, HIGH);
 distance = duration*0.034/2;
 Serial.print("Distance: ");
